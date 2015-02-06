8edcf9455bdec437f09dbb080eb9f6a966c887a8
Eric Hodgins <hodgins.e@gmail.com>
2015-02-05 22:14:01 -0500

    Added hashing secure user

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ import os
 import jinja2
 import re
 
+import random
+import hashlib
+import hmac
+from string import letters
+
+
+from google.appengine.ext import db
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
@@ -44,6 +52,9 @@ def render_str(template, **params):
 #  main handlers
 
 class MainHandler(webapp2.RequestHandler):
+	def get(self):
+		self.redirect('/signup')
+
 	def write(self, *a, **kw):
 		self.response.write(*a, **kw)
 
@@ -62,46 +73,129 @@ class Signup(MainHandler):
 		self.render("signup.html")
 
 	def post(self):
-		username = self.request.get('username')
-		password = self.request.get('password')
-		verify = self.request.get('verify')
-		email = self.request.get('email')
+		self.username = self.request.get('username')
+		self.password = self.request.get('password')
+		self.verify = self.request.get('verify')
+		self.email = self.request.get('email')
 
-		params = dict(username=username, email=email)
+		params = dict(username=self.username, email=self.email)
 
 		#  check form inputs
 		check_error = False
-		if not valid_username(username):
+		if not valid_username(self.username):
 			params['error_username'] = "That's not a valid username"
 			check_error = True
-		if not valid_password(password):
+		if not valid_password(self.password):
 			params['error_password'] = "That's not a valid password"
 			check_error = True
-		elif password != verify:
+		elif self.password != self.verify:
 			params['error_verify'] = "Passwords don't match"
 			check_error = True
 
-		if not valid_email(email):
+		if not valid_email(self.email):
 			params['error_email'] = "That's not a valid email"
 			check_error = True
 
 		if check_error:
 			self.render("signup.html", **params)
 		else:
-			self.redirect('/welcome')
+			if not self.check_if_user_exists():  # Also adds the user if it doesn't exist.
+				self.redirect('/welcome')
+
+	def check_if_user_exists(self):
+		u = User.by_name(self.username)
+		if u:
+			err_msg = "Sorry, that user already exists."
+			self.render("signup.html", error_username=err_msg)
+			return True
+		else:
+			u = User.register(self.username, self.password, self.email)
+			u.put()
+			return False
 
 
 
 class Welcome(MainHandler):
 	def get(self):
-		self.write("Hello")
+		self.render("welcome.html")
+
+
+class Login(MainHandler):
+	def get(self):
+		self.render("login.html")
+
+	def post(self):
+		username = self.request.get('username')
+		password = self.request.get('password')
+
+		if User.login(username, password):
+			self.redirect('/welcome')
+		else:
+			err_msg = "Sorry that's an invalid password or username."
+			self.render("login.html", error_username=err_msg)
+
+
+
+
+#  Hashing functions to secure user at Signup
+def make_salt(length=5):
+	return ''.join(random.choice(letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt=None):
+	if not salt:
+		salt = make_salt()
+	h = hashlib.sha256(name + pw + salt).hexdigest()
+	return '%s,%s' % (salt, h)
+
+#  Checking User at login. Verify everthing's good
+def valid_pw(name, pw, h):
+	salt = h.split(',')[0]
+	if h == make_pw_hash(name, pw, salt):
+		return True
+	else:
+		return False
+
+def secure_cookie(name, val):
+	cookie_val = 
+
+
+# Data Schema for the users
+class User(db.Model):
+	name = db.StringProperty(required=True)
+	pw_hash = db.StringProperty(required=True)
+	email = db.StringProperty()
+	movie = db.StringProperty()
+	available = db.StringProperty()
+
+	@classmethod
+	def by_name(cls, name):
+		u = User.all().filter('name =', name).get()
+		return u
+
+	@classmethod
+	def register(cls, name, pw, email=None):
+		pw_hash = make_pw_hash(name, pw)
+		return User(name=name,
+			        pw_hash=pw_hash,
+			 		email=email)
+
+	@classmethod
+	def login(cls, name, pw):
+		u = User.all().filter('name =', name).get()
+		if u:
+			if valid_pw(name, pw, u.pw_hash):
+				return True
+		else:
+			return False
+
 
 
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler),
 	('/signup', Signup),
-	('/welcome', Welcome)
+	('/welcome', Welcome),
+	('/login', Login)
 ], debug=True)
 
 
